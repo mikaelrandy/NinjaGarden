4707f0ba83d089cf9d5dd9dcf99938c219760c66
Mikael RANDY <mikael.randy@gmail.com>
2012-03-24 15:19:31 +0100

    Base for game manager

---
+++ server/game/game.js
@@ -0,0 +1,22 @@
+
+
+/**
+ *	Game manager
+ * 	
+ * 	Will manage game participation
+ */
+Game = function () {
+	this.playerStack = [];
+}
+
+Game.prototype = {
+
+	init: function() {
+		this.playerStack = [];
+	},
+
+	connect: function() {
+		
+	},
+
+};
