7a02c8e063431d60ae5e36d971122db949c25f5e
Christophe Dolivet <cdolivet@hotmail.com>
2012-03-24 23:07:04 +0100

    Improve usage exp

--- server/decision/director.js
+++ server/decision/director.js
@@ -130,8 +130,12 @@ Director.prototype = {
 	// compute the result of a smoke
 	// it just stop the player and add an event on it
 	computeSmokeAction: function(ninja, decision) {
-		ninja.character.addEvent(Config.Events.SMOKE);
 		ninja.character.state = null;	// a l'arret
+		
+		if(ninja.character.stats.smokesLeft > 0) {
+			ninja.character.stats.smokesLeft--;
+			ninja.character.addEvent(Config.Events.SMOKE);
+		}
 	},
 
 	// find ninja arround the given point
