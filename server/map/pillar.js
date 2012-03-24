498119d60392c10c9ba862cf724a85dfbf29d708
Christophe Dolivet <cdolivet@hotmail.com>
2012-03-24 19:58:02 +0100

    Add map classes

---
+++ server/map/pillar.js
@@ -0,0 +1,15 @@
+Pillar = function(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+Pillar.prototype = {
+	/**
+	 *	Pillar initialisation
+	 */
+	init: function() {
+		
+	},
+};
+
+exports.Pillar = Pillar;
