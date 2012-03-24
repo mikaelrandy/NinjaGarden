01928cfd76315dedcbbf964a1341757a73fc7c4f
Christophe Dolivet <cdolivet@hotmail.com>
2012-03-24 12:26:36 +0100

    Improve character class

--- server/character/character.js
+++ server/character/character.js
@@ -1,9 +1,28 @@
-function character() {
+function Character(id) {
+	this.id = id;
 	this.init();
 }
 
-character.prototype = {
+Character.states = ['walk', 'stop', 'dead', 'stunt'];
+Character.dirs = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+
+
+Character.prototype = {
+	rand: function(max) {
+  		return Math.abs(Math.round(Math.random() * upper));
+	},
 	init: function() {
-		this.x = 15;
+		this.x = this.rand(0, 50);	// TODO: replace with max map coord
+		this.y = this.rand(0, 100);	// TODO: replace with max map coord
+		this.dir = Character.dirs[this.rand(Character.dirs.length)];
+		this.state = Character.states[this.rand(Character.states.length)];
+		this.stats = {
+			'kills' : {},
+			'smokes' : {},
+			'pillars' : {},
+			'stunts' : {}
+		};
+		this.decisionStack : [];
+		this.events: [];
 	}
 }
